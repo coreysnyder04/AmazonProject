c8244c51e7ed821a3522c55a225efd2ff5269e4f
unknown <Pro@.(none)>
2011-05-02 20:22:04 -0400

    Changed amazon service.

--- AmazonProject/application/controllers/IndexController.php
+++ AmazonProject/application/controllers/IndexController.php
@@ -33,7 +33,7 @@ class IndexController extends Zend_Controller_Action
 		//Hickman do your thing w/ this item ID to pull back the data from amazon
 		$itemID = $this->getRequest()->getParam("itemID");
 		
-		$itemData = $this->amazonService->getAmazonData($itemID);
+		$itemData = @$this->amazonService->getAmazonData($itemID);
 
 		//this is for testing - will remove for production
 		foreach($itemData as $d)
--- AmazonProject/application/models/AmazonService.php
+++ AmazonProject/application/models/AmazonService.php
@@ -1,20 +1,8 @@
 <?php
-/*
-require_once 'GamesTable.php';
-require_once 'LeaguesTable.php';
-require_once 'LocationsTable.php';
-require_once 'SessionsTable.php';
-require_once 'TeamsTable.php';
-*/
-
 class Default_Model_AmazonService
 {
 	protected $db;
-	protected $games;
-	protected $leagues;
-	protected $locations;
-	protected $sessions;
-	protected $teams;
+	protected $itemTable;
 	
 	function __construct()
 	{
@@ -29,29 +17,64 @@ class Default_Model_AmazonService
 		
 		$this->db = Zend_Db::factory($config->db->adapter, $options);
 		Zend_Db_Table_Abstract::setDefaultAdapter($this->db);
+		$this->itemTable = new Zend_Db_Table('item_lookups');
 	}
 	
 	public function getAmazonData($itemID)
-	{
+	{	
 		$client = new Zend_Service_Amazon('AKIAJVBMLHZM5WWDE52A','US','2pz5jsxWFfkFbWQLkveKmrmnDwZSbvdWZNzfEzcQ');
 		$item = $client->itemLookup($itemID,array(
 		'ResponseGroup' => 'Medium,Offers'
 		));
-		
+						
 		$itemData = "";
 		$itemData = array(
 			'Title' => $item->Title,
-			'CurrentPrice' => $item->FormattedPrice,
+			'ListPrice' => $item->FormattedPrice,
+			'LowestNewPrice' => $item->Offers->LowestNewPrice,
 			'LowestUsedPrice' => $item->Offers->LowestUsedPrice,
 			'StockNew' => $item->Offers->TotalNew,
 			'StockUsed' => $item->Offers->TotalUsed,
 			'SmallImageUrl' => $item->SmallImage->Url,
 			'MediumImageUrl' => $item->MediumImage->Url,
 			'LargeImageUrl' => $item->LargeImage->Url,
-			'AmazonUrl' => $item->DetailPageURL
+			'AmazonUrl' => $item->DetailPageURL,
+			'Manufacturer' => $item->Manufacturer
 		);
 		
+		
+		//get and format variables for database input
+		$currentDate = date("Y-m-d");		
+		$ListPriceNoDollarSign = preg_replace('/[\$,]/', '', $itemData[ListPrice]);
+		
+		$writeToDb = true;
+		if($writeToDb == true)
+		{
+			$data = array(
+					'item_id' => 'NULL' ,
+				  	'asin' => $itemID,
+				  	'last_lookup_date' => $currentDate,
+				 	'title' => $itemData[Title],
+				 	'manufacturer' => $itemData[Manufacturer],
+				  	'lowest_new_price' => $itemData[LowestNewPrice],
+				   	'lowest_used_price' => $itemData[LowestUsedPrice],
+				    'list_price' => $ListPriceNoDollarSign,
+				    'amazon_url' => "$itemData[AmazonUrl]"
+			);
+			
+			$this->itemTable->insert($data);
+		}
+		
 		return $itemData;
+	}
+	
+	public function testDatabase()
+	{
+	
+		$sql = "SELECT item_id FROM item_lookups WHERE id = '1'";
+		
+		return $this->db->fetchAll($sql);
+	
 	}
 	
 	public function getLeagueNameByAlias($alias)
